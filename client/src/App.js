844d97533b94a5a61900ad151d2895adc9fa736d
Gareth Leenderts <garethleenderts@gmail.com>
2022-03-17 14:01:32 +0200

    Added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import React from 'react';
 // import './App.css';
 import RegistrationForm from './Components/register.js';
 import LoginForm from './Components/login.js';
+import Home from './Components/Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
   return (
-    <div>
-      <p>Hello</p>
-      <RegistrationForm/>
-      <LoginForm/>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path={["/", "/home"]} element={<Home />}></Route> */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/register" element={<RegistrationForm />}></Route>
+        <Route path="/login" element={<LoginForm />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
