0f97b7037d0a69a51cd3fad77673e4cbe88dd889
Gareth Leenderts <garethleenderts@gmail.com>
2022-03-18 09:56:46 +0200

    Added registration functionality

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const cors = require('cors');
 // const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt'); // password hashing library
+
+
+// going to store users in a local variable for this tutorial
+const users = []
+
 
 // Instantiate server
 const app = express();
@@ -20,6 +26,33 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: false }));  alternate implementation
 
 // Middleware END --------
+
+
+// ROUTE HANDLING //
+app.post("/login", (req, res) => {});
+
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        res.redirect("/login");
+    } catch (error) {
+        console.log(error);
+        res.redirect("/register");
+    }
+
+    console.log(users);
+    
+});
+// ROUTE HANDLING END //
+
 
 
 // Set Port and Connect
